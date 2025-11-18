da4796a837b8cdbab4a0a11c23edd8c000d55df8
PYJ0915 <qkrdbwls3542@gmail.com>
2025-11-18 22:37:56 +0900

    1118 집 오류방지

--- ServletProject/src/main/java/edu/kh/servlet/controller/ExampleController3.java
+++ ServletProject/src/main/java/edu/kh/servlet/controller/ExampleController3.java
@@ -10,43 +10,43 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/coffee")
-public class ExampleController3 extends HttpServlet{
-	
+public class ExampleController3 extends HttpServlet {
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		String orderer = req.getParameter("orderer");
 		String coffee = req.getParameter("coffee");
 		String type = req.getParameter("type");
-		
+
 		String option = req.getParameter("opt");
 		String[] optArr = req.getParameterValues("opt");
 		// req.getParameter("name속성값")
 		// : 같은 name 속성을 가진 여러 개 값 중 첫 번째 값만 반환
-		
+
 		// req.getParameterValues("name속성값")
 		// : 같은 name 속성을 가진 모든 값을 배열(String[]) 형태로 반환
-		
+
 		System.out.println(orderer);
 		System.out.println(coffee);
 		System.out.println(type);
 		System.out.println(option); // 첫 번째 값인 shot만 반환
 		System.out.println(optArr); // 주소 값
-		
+
 		// opt 미선택 시 optArr == null
-		
-		if(optArr != null) {
-			
-			for(String opt : optArr) {
+
+		if (optArr != null) {
+
+			for (String opt : optArr) {
 				System.out.println(opt);
 			}
-			
+
 		}
-		
+
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/result2.jsp");
-		
+
 		dispatcher.forward(req, resp);
-		
+
 	}
 
 }
